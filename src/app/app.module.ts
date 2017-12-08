1749865613245dcbe426a5134dff30fe6cf606b7
Kyle Sherbert <ksherb1@students.towson.edu>
2017-12-08 13:30:57 -0500

    Set up ListView

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ListViewComponent } from './list-view/list-view.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListViewComponent
   ],
   imports: [
     BrowserModule
