a8d91705a7fc7010d672e8c8fcd9a047ab9f34b3
Jewel7 <volleyballjewel@gmail.com>
2017-12-08 14:44:49 -0500

    edit home-view component file

--- src/app/home-view/home-view.component.ts
+++ src/app/home-view/home-view.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ToDoItem } from '../models/todo-item';
+import { ToDoService } from '../resources/services/to-do.service';
+
 
 @Component({
   selector: 'app-home-view',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-view.component.css']
 })
 export class HomeViewComponent implements OnInit {
+  list: ToDoItem[]; //this is a list property that returns an array of lists that it acquires from a service
 
-  constructor() { }
+
+  constructor(private toDoService :ToDoService) { }
+
+
 
   ngOnInit() {
   }
+  fetchToDos() {
+    this.toDoService.getLists();
+  }  
 
 }
