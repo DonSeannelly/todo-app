fea18518c4c46ca54cc0622ed88718dec73f586e
Jewel7 <volleyballjewel@gmail.com>
2017-12-08 14:35:26 -0500

    change home component file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeViewComponent } from './home-view/home-view.component';
 
 
 export const router: Routes = [
     // ADD ROUTES TO COMPONENTS HERE
+    { path: '', component: HomeViewComponent },
     { path: '**', redirectTo: '' },
 ];
 
